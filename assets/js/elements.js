5d1b201b0c3ecb38bc9a459aa8406741057e1887
Mychol Zhou <mychol.zhou@gmail.com>
2023-07-14 16:10:58 +0200

    divisione del file JS

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,4 +1,4 @@
-
+// ELEMENTI CREATI ATTRAVERSO JS
 
 const body = document.querySelector('body')
 
@@ -109,69 +109,5 @@ scoreBoard.appendChild(console)
     divBottoniGuest.appendChild(bottonePlusGuest);
     divBottoniGuest.appendChild(bottoneMinusGuest)
     divBottoniGuest.appendChild(bottoneResetPunteggioGuest)
-
-
-// Plus dello score fino al valore 99
-
-bottonePlusHome.addEventListener('click', () => {
-  if (valueHome < 99) {
-    valueHome++;
-    scoreHome.innerHTML = valueHome;
-  };
-  if (valueHome < 10 ) {
-    let valueString = valueHome.toString();
-    scoreHome.innerHTML = valueString.padStart(2,0)
-  }
-})
-
-bottonePlusGuest.addEventListener('click', () => {
-  if (valueGuest < 99) {
-    valueGuest++;
-    scoreGuest.innerHTML = valueGuest;
-  };
-  if (valueGuest < 10 ) {
-    let valueString = valueGuest.toString();
-    scoreGuest.innerHTML = valueString.padStart(2,0)
-  }
-})
-
-// Minus dello score che non puo andare sotto lo 0 
-
-bottoneMinusHome.addEventListener('click', () => {
-  if (valueHome > 0) {
-    valueHome--;
-    scoreHome.innerHTML = valueHome;
-  };
-  if (valueHome < 10 ) {
-    let valueString = valueHome.toString();
-    scoreHome.innerHTML = valueString.padStart(2,0)
-  }
-})
-
-bottoneMinusGuest.addEventListener('click', () => {
-  if (valueGuest > 0) {
-    valueGuest--;
-    scoreGuest.innerHTML = valueGuest;
-  };
-  if (valueGuest < 10 ) {
-    let valueString = valueGuest.toString();
-    scoreGuest.innerHTML = valueString.padStart(2,0)
-  }
-})
-
-//Reset del punteggio a 0
-
-bottoneResetPunteggioHome.addEventListener('click', () => {
-  valueHome = "00";
-  scoreHome.innerHTML = valueHome;
-})
-
-bottoneResetPunteggioGuest.addEventListener('click', () => {
-  valueGuest = "00";
-  scoreGuest.innerHTML = valueGuest;
-})
-
-
-
 
 
